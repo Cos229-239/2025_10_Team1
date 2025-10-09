3b046ed6768f540053c1f985a607e8405542ebb2
Timothy Jackson <TAJackson5@student.fullsail.edu>
2025-10-09 17:57:22 -0500

    Started first lines of coding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +20,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MyApplicationTheme {
+                    PlannerScreen(modifier = Modifier.fillMaxSize())
+                }
                 }
             }
         }
@@ -31,17 +30,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun PlannerScreen(modifier: Modifier = Modifier) {
+    // Scaffold provides the basic material design layout structure
+    Scaffold(
+        bottomBar = {
+            // We'll define the BottomNavigationBar later
+        },
         modifier = modifier
-    )
+    ) { innerPadding ->
+        // Main content goes here, using the padding from the Scaffold
+        Column(
+            modifier = Modifier
+                .padding(innerPadding) // Important for content not to overlap with bars
+                .padding(horizontal = 16.dp) // Add horizontal padding to the whole screen
+        ) {
+            // All the screen components will be called here
+            TopHeader()
+            WelcomeMessage(name = "Name")
+            DaySelector()
+        }
+    }
+}
+
+@Composable
+fun DaySelector() {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun WelcomeMessage(name: String) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun TopHeader() {
+    TODO("Not yet implemented")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PlannerScreenPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        PlannerScreen()
     }
 }
